0c5463a2332bd1db4296c2ebfc0e92cb326c0f7a
JonathanGuillenDev <jonathanguillencontact@gmail.com>
2025-07-18 12:22:39 -0400

    removed backend service

--- src/views/Messages.vue
+++ src/views/Messages.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="messages">
-    <topic-details v-bind:topic-id="parseInt($route.params.id)" />
+    <topic-details :topic-id="topicId" />
   </div>
 </template>
 
@@ -8,9 +8,18 @@
 import TopicDetails from "@/components/TopicDetails.vue";
 
 export default {
-  name: "Messages",
+  name: "Messages", // This component's name
+  // IMPORTANT: Since the router config for the 'Messages' route has 'props: true',
+  // this component (Messages.vue) *must* declare 'topicId' as a prop
+  // to receive the route parameter.
+  props: {
+    topicId: [Number, String] // Declare topicId as a prop, as expected from the router
+  },
   components: {
     TopicDetails
+  },
+  created() {
+    console.log('Messages.vue created. Received topicId prop from router:', this.topicId);
   }
 };
 </script>
